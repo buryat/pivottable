15628f3a29137ca0748f10a141b6825d827b7827
Vadim Semenov <_@pizd.ec>
2013-07-15 09:04:15 -0400

    Filter fix

--- pivottable/PivotUI.js
+++ pivottable/PivotUI.js
@@ -61,68 +61,102 @@ define(
 
             effectNames = _.keys(options.effects)
 
-            uiTable = $("<table class='table table-bordered' cellpadding='5'>")
+            refresh = function() {
+                var effect,
+                    exclusions = [],
+                    vals = [],
+                    subopts = {
+                        derivedAttributes: options.derivedAttributes,
+                        cols: [],
+                        rows: []
+                    }
+
+                $(".pivottable-rows li > span").each(function() {
+                    return subopts.rows.push($(this).text())
+                })
+                $(".pivottable-cols li > span").each(function() {
+                    return subopts.cols.push($(this).text())
+                })
+                $(".pivottable-vals li > span").each(function() {
+                    return vals.push($(this).text())
+                })
+                subopts.aggregator = options.aggregators[aggregator.val()](vals)
+                $('input.pivottable-filter').not(':checked').each(function() {
+                    return exclusions.push($(this).data("filter"))
+                })
+                subopts.filter = function(row) {
+                    for (var i = 0, l = exclusions.length; i < l; i++) {
+                        var exclusion = exclusions[i],
+                            k = exclusion[0],
+                            v = exclusion[1]
+
+                        if (row[k] == v) {
+                            return false
+                        }
+                    }
+
+                    return true
+                }
+                if (effectNames.length) {
+                    effect = $('input[name=effects]:checked').val()
+                    if (effect !== "None") {
+                        subopts.postProcessor = options.effects[effect]
+                    }
+                }
+
+                subopts.container = pivotTable
+                subopts.data = input
+
+                new Pivot(subopts)
+            }
+
+            uiTable = $("<table class='pivottable table-bordered'>")
 
             if (effectNames.length) {
                 effectNames.unshift("None")
-                controls = $("<td colspan='2' align='center'>")
-                form = $("<form>").addClass("form-inline")
+                controls = $("<td colspan='2'>")
+                form = $("<form class='form-inline pivottable-effects'>")
                 controls.append(form)
                 form.append($("<strong>").text("Effects:"))
 
                 effectNames.forEach(function(x) {
-                    radio = $("<input type='radio' name='effects' id='effects_" + (x.replace(/\s/g, "")) + "'>")
-                        .css({
-                            "margin-left": "15px",
-                            "margin-right": "5px"
-                        }).val(x)
+                    radio = $("<input type='radio' name='effects' id='pivottable-effects_" + (x.replace(/\s/g, "")) + "'>").val(x)
                     if (x === "None") {
                         radio.attr("checked", "checked")
                     }
                     form.append(radio)
                         .append(
-                            $("<label class='checkbox inline' for='effects_" + (x.replace(/\s/g, "")) + "'>").text(x)
+                            $("<label class='checkbox inline' for='pivottable-effects_" + (x.replace(/\s/g, "")) + "'>").text(x)
                         )
                 })
                 uiTable.append($("<tr>").append(controls))
             }
 
-            colList = $("<td colspan='2' id='unused' class='pvtAxisContainer pvtHorizList'>")
+            colList = $("<td colspan='2' class='pivottable-axis-container pivottable-horizontal-list'>")
 
             tblCols.forEach(function(c) {
                 if (options.hiddenAxes.indexOf(c) < 0) {
                     (function(c) {
                         var btns, colLabel, filterItem, numKeys, valueList
                         numKeys = Object.keys(axisValues[c]).length
-                        colLabel = $("<nobr>").text(c)
-                        valueList = $("<div>").css({
-                            "z-index": 100,
-                            "width": "280px",
-                            "height": "350px",
-                            "overflow": "scroll",
-                            "border": "1px solid gray",
-                            "background": "white",
-                            "display": "none",
-                            "position": "absolute",
-                            "padding": "20px"
-                        })
+                        colLabel = $("<span>").text(c)
+                        valueList = $("<div class='pivottable-values-list'>")
                         valueList.append($("<strong>").text("" + numKeys + " values for " + c))
                         if (numKeys > 20) {
                             valueList.append($("<p>").text("(too many to list)"))
                         } else {
                             btns = $("<p>")
                             btns.append($("<button>").text("Select All").bind("click", function() {
-                                return valueList.find("input").attr("checked", true)
+                                return valueList.find("input").prop("checked", true)
                             }))
                             btns.append($("<button>").text("Select None").bind("click", function() {
-                                return valueList.find("input").attr("checked", false)
+                                return valueList.find("input").prop("checked", false)
                             }))
                             valueList.append(btns)
                             Object.keys(axisValues[c]).sort().forEach(function(k) {
                                 var v = axisValues[c][k]
                                 filterItem = $("<label>")
-                                filterItem.append($("<input type='checkbox' class='pvtFilter'>").attr("checked",
-                                    true).data("filter", [c, k]))
+                                filterItem.append($("<input type='checkbox' class='pivottable-filter'>").attr("checked", true).data("filter", [c, k]))
                                 filterItem.append($("<span>").text("" + k + " (" + v + ")"))
                                 valueList.append($("<p>").append(filterItem))
                             })
@@ -150,92 +184,41 @@ define(
             })
             uiTable.append($("<tr>").append(colList))
             tr1 = $("<tr>")
-            aggregator = $("<select class='pivottable-aggregator-select'>").css("margin-bottom", "5px").bind("change", function() {
-                refresh()
-            })
+            aggregator = $("<select class='pivottable-aggregator-select'>").css("margin-bottom", "5px").bind("change", refresh)
             _.each(options.aggregators, function(_, x) {
                 aggregator.append($("<option>").val(x).text(x))
             })
-            tr1.append($("<td id='vals' class='pvtAxisContainer pvtHorizList'>").css("text-align",
-                "center").append(aggregator).append($("<br>")))
-            tr1.append($("<td id='cols' class='pvtAxisContainer pvtHorizList'>"))
+            tr1.append($("<td class='pivottable-vals pivottable-axis-container pivottable-horizontal-list'>").append(aggregator))
+            tr1.append($("<td class='pivottable-cols pivottable-axis-container pivottable-horizontal-list'>"))
             uiTable.append(tr1)
             tr2 = $("<tr>")
-            tr2.append($("<td valign='top' id='rows' class='pvtAxisContainer'>"))
-            pivotTable = $("<td valign='top'>")
+            tr2.append($("<td class='pivottable-rows pivottable-axis-container'>"))
+            pivotTable = $("<td>")
             tr2.append(pivotTable)
             uiTable.append(tr2)
             containerEl.html(uiTable)
 
             options.cols.forEach(function(x) {
-                $("#cols").append($("#axis_" + (x.replace(/\s/g, ""))))
+                $(".pivottable-cols").append($("#axis_" + (x.replace(/\s/g, ""))))
             })
             options.rows.forEach(function(x) {
-                $("#rows").append($("#axis_" + (x.replace(/\s/g, ""))))
+                $(".pivottable-rows").append($("#axis_" + (x.replace(/\s/g, ""))))
             })
             options.vals.forEach(function(x) {
-                $("#vals").append($("#axis_" + (x.replace(/\s/g, ""))))
+                $(".pivottable-vals").append($("#axis_" + (x.replace(/\s/g, ""))))
             })
             if (options.aggregatorName != null) {
-                $("#aggregator").val(options.aggregatorName)
+                $(".pivottable-aggregator-select").val(options.aggregatorName)
             }
             if (options.effectsName != null) {
-                $("#effects_" + (options.effectsName.replace(/\s/g, ""))).attr('checked', true)
-            }
-            refresh = function() {
-                var effect,
-                    exclusions = [],
-                    vals = [],
-                    subopts = {
-                        derivedAttributes: options.derivedAttributes,
-                        cols: [],
-                        rows: []
-                    }
-
-                $("#rows li nobr").each(function() {
-                    return subopts.rows.push($(this).text())
-                })
-                $("#cols li nobr").each(function() {
-                    return subopts.cols.push($(this).text())
-                })
-                $("#vals li nobr").each(function() {
-                    return vals.push($(this).text())
-                })
-                subopts.aggregator = options.aggregators[aggregator.val()](vals)
-                $('input.pvtFilter').not(':checked').each(function() {
-                    return exclusions.push($(this).data("filter"))
-                })
-                subopts.filter = function(row) {
-                    for (var i = 0, l = exclusions.length; i < l; i++) {
-                        var exclusion = exclusions[i],
-                            k = exclusion[0],
-                            v = exclusion[1]
-
-                        if (row[k] === v) {
-                            return false
-                        }
-                    }
-
-                    return true
-                }
-                if (effectNames.length) {
-                    effect = $('input[name=effects]:checked').val()
-                    if (effect !== "None") {
-                        subopts.postProcessor = options.effects[effect]
-                    }
-                }
-
-                subopts.container = pivotTable
-                subopts.data = input
-
-                new Pivot(subopts)
+                $("#pivottable-effects_" + (options.effectsName.replace(/\s/g, ""))).attr('checked', true)
             }
 
             refresh()
             $('input[name=effects]').bind("change", refresh)
-            $(".pvtAxisContainer")
+            $(".pivottable-axis-container")
                 .sortable({
-                    connectWith: ".pvtAxisContainer",
+                    connectWith: ".pivottable-axis-container",
                     items: 'li'
                 })
                 .bind("sortstop", refresh)
